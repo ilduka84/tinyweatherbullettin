d05b96faf67391654f99304c5a4f84f3c7e862aa
yonni <yonni.sciunnach@sourcesense.com>
2020-10-13 20:44:43 +0200

    controller implementation generic error entities

--- src/main/java/com/faire/ai/tinyweatherbulletin/controllers/ThreeDaysForecastController.java
+++ src/main/java/com/faire/ai/tinyweatherbulletin/controllers/ThreeDaysForecastController.java
@@ -1,12 +1,15 @@
 package com.faire.ai.tinyweatherbulletin.controllers;
 
+import com.faire.ai.tinyweatherbulletin.controllers.utils.WeatherBullettinsUtils;
 import com.faire.ai.tinyweatherbulletin.entities.WeatherBullettins;
 import com.faire.ai.tinyweatherbulletin.services.IWeather;
+import com.faire.ai.tinyweatherbulletinapi.dom.GenericError;
 import com.faire.ai.tinyweatherbulletinapi.dom.ThreeDaysForecast;
 import com.faire.ai.tinyweatherbulletinapi.rest.ThreeDaysForecastApi;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,20 +25,25 @@ public class ThreeDaysForecastController implements ThreeDaysForecastApi {
     @Autowired
     IWeather weatherService;
 
+    @Value("${weather.workinghours}")
+    List<String> workingHours;
+
     @Override
-    public ResponseEntity<ThreeDaysForecast> getThreeDaysForecast(@ApiParam(value = "country",required=true, defaultValue = "IT") @PathVariable("country") String country
+    public ResponseEntity getThreeDaysForecast(@ApiParam(value = "country",required=true, defaultValue = "IT") @PathVariable("country") String country
             , @ApiParam(value = "city",required=true, defaultValue = "Milan") @PathVariable("city") String city
     ){
 
         try {
             WeatherBullettins weatherBullettins = weatherService.getWeatherBullettinsFromCityAndCountry(city, country);
+            ThreeDaysForecast threeDaysForecast = WeatherBullettinsUtils.fromWeatherBullettinsTo(weatherBullettins,workingHours);
+            return new ResponseEntity<>(threeDaysForecast,HttpStatus.OK);
         }catch(WebClientResponseException e){
-
+            GenericError genericError = new GenericError();
+            genericError.setCod(e.getRawStatusCode());
+            genericError.setMessage(e.getMessage());
+            return new ResponseEntity(genericError, e.getStatusCode());
 
         }
-
-
-            return null;
     }
 
 }
